26146ed2600a6d5fd6977e620c9de50cea753797
BS-GavinC <98208007+VZGLY@users.noreply.github.com>
2023-11-14 11:10:56 +0100

    Initial commit

---
+++ ASPMVCWebAPI/Controllers/UserController.cs
@@ -0,0 +1,44 @@
+﻿using ASPMVCWebAPI.Context;
+using ASPMVCWebAPI.Models;
+using Microsoft.AspNetCore.Mvc;
+
+namespace ASPMVCWebAPI.Controllers
+{
+    public class UserController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        public IActionResult Details(int id)
+        {
+            User user = FakeDB.Users.Where(x => x.Id == id).SingleOrDefault();
+
+
+            if (user == null)
+            {
+                return View("NotFound");
+            }
+
+            return View(user);
+        }
+
+        public IActionResult Create()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public IActionResult Create(UserCreateViewModel createViewModel)
+        {
+            if (ModelState.IsValid)
+            {
+                Console.WriteLine("Super formulaire valid toosa toosa");
+                return RedirectToAction("Index");
+            }
+            Console.WriteLine("Aaaaah mec... Pas ouf...");
+            return View(createViewModel);
+        }
+    }
+}
